4f3d37a417ed3ded8ff84a209f4fcfb65e30cb21
Nauman <naumanumer10@outlook.com>
2023-08-03 22:23:36 +0100

    Style: Navbar and Footer Enhancement

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,6 +10,7 @@
     <input className='menu-btn' type='checkbox' id='menu-btn'/>
     <label className='menu-icon' for='menu-btn'>
       <span className='nav-icon'></span>
+      
     </label>
 
     <ul className='menu'>
